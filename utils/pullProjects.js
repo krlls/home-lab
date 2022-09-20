0f061b05cb845a94c3b2f9a882e771532c20f5dd
Kirill Smirnov <krrllsm@gmail.com>
2022-09-20 18:07:45 +0400

    split to files

--- utils/pullProjects.js
+++ utils/pullProjects.js
@@ -4,7 +4,7 @@ const projects = require('../components.json').projects
 const exec = require('child_process').exec
 
 function startProjects(projectsList) {
-  console.log("\x1b[31m", '== PROJECTS PULL ==','\x1b[0m')
+  console.log("\x1b[33m", '== PROJECTS PULL ==','\x1b[0m')
   console.log('Projects:', "\x1b[32m", ...projectsList.map((p) => p.name),'\x1b[0m')
   const cmpStr = projectsList.reduce((previousValue, { file }) => {
     return previousValue + ` -f ./projects/${file}`
@@ -22,7 +22,7 @@ function startProjects(projectsList) {
     }
 
     if (!err) {
-      console.log("\x1b[31m", '== PULL SUCCESS ==','\x1b[0m')
+      console.log("\x1b[33m", '== PULL SUCCESS ==','\x1b[0m')
     }
   })
 }
