ae809a6404bd95d1e477ede5f3c35420e9cb89a2
ksmi <krrllsm@gmail.com>
2022-09-20 18:12:42 +0400

    Split projects (#14)
    * split to files

--- utils/pullProjects.js
+++ utils/pullProjects.js
@@ -4,8 +4,8 @@ const projects = require('../components.json').projects
 const exec = require('child_process').exec
 
 function startProjects(projectsList) {
-  console.log("\x1b[31m", '== PROJECTS PULL ==','\x1b[0m')
-  console.log('Projects:', "\x1b[32m", ...projectsList.map((p) => p.name),'\x1b[0m')
+  console.log("\x1b[33m", '== PROJECTS PULL ==','\x1b[0m')
+  console.log('Projects:', "\x1b[32m", projectsList.map((p) => p.name).join(', '),'\x1b[0m')
   const cmpStr = projectsList.reduce((previousValue, { file }) => {
     return previousValue + ` -f ./projects/${file}`
   }, '')
@@ -17,10 +17,12 @@ function startProjects(projectsList) {
       console.log(err?.message)
     }
 
-    console.log(stdout)
+    if (stdout) {
+      console.log(stdout)
+    }
 
     if (!err) {
-      console.log("\x1b[31m", '== PULL SUCCESS ==','\x1b[0m')
+      console.log("\x1b[33m", '== PULL SUCCESS ==','\x1b[0m')
     }
   })
 }
--- utils/stopProject.js
+++ utils/stopProject.js
@@ -5,12 +5,12 @@ const exec = require('child_process').exec
 
 function stopProject(projectsList) {
   const project = projectsList.find((p) => p.name === process.argv[2])
-  const projectNames = projectsList.map((p) => p.name)
+  const projectNames = projectsList.map((p) => p.name).join(', ')
 
   if (!project) {
 
     console.error("\x1b[31m", 'ERROR: Select project name: `make component.stop component=<name>`', '\x1b[0m')
-    console.log('\x1b[33m', 'Names:', projectNames.join(', '), '\x1b[0m')
+    console.log('\x1b[33m', 'Names:', projectNames, '\x1b[0m')
     return
   }
 
@@ -26,7 +26,9 @@ function stopProject(projectsList) {
       console.log(err?.message)
     }
 
-    console.log(stdout)
+    if (stdout) {
+      console.log(stdout)
+    }
 
     if (!err) {
       console.log("\x1b[31m", '== STOP SUCCESS ==','\x1b[0m')
