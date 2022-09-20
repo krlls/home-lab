c0fd761aa65f1194ea119bfcefd38f3857f39eac
ksmi <krrllsm@gmail.com>
2022-09-20 17:51:38 +0400

    New structure (#13)
    * new structure

---
+++ utils/printProjectNames.js
@@ -0,0 +1,9 @@
+/*jshint esversion: 7 */
+
+const projects = require('../components.json').projects
+
+function printProjectNames(projectsList) {
+  console.log('Projects:', "\x1b[32m", projectsList.map((p) => p.name).join(', '),'\x1b[0m')
+}
+
+printProjectNames(projects)
---
+++ utils/startProjects.js
@@ -0,0 +1,28 @@
+/*jshint esversion: 7 */
+
+const projects = require('../components.json').projects
+const exec = require('child_process').exec
+
+function startProjects(projectsList) {
+  console.log("\x1b[35m", '== PROJECTS START ==','\x1b[0m')
+  console.log('Projects:', "\x1b[32m", ...projectsList.map((p) => p.name),'\x1b[0m')
+  const cmpStr = projectsList.reduce((previousValue, { file }) => {
+    return previousValue + ` -f ./projects/${file}`
+  }, '')
+
+  const cmd = `docker-compose${cmpStr} --env-file ./config/.env up -d`
+
+  exec(cmd, (err, stdout) => {
+    if (err) {
+      console.log(err?.message)
+    }
+
+    console.log(stdout)
+
+    if (!err) {
+      console.log("\x1b[35m", '== START SUCCESS ==','\x1b[0m')
+    }
+  })
+}
+
+startProjects(projects)
