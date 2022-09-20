ce46025a7ada14a81a6666f0bbf51ddcf90b605c
ksmi <krrllsm@gmail.com>
2022-09-20 18:12:42 +0400

    Split projects (#14)
    * split to files

--- utils/pullProject.js
+++ utils/pullProject.js
@@ -5,11 +5,11 @@ const exec = require('child_process').exec
 
 function pullProject(projectsList) {
   const project = projectsList.find((p) => p.name === process.argv[2])
-  const projectNames = projectsList.map((p) => p.name)
+  const projectNames = projectsList.map((p) => p.name).join(', ')
 
   if (!project) {
     console.error("\x1b[31m", 'ERROR: Select project name', '\x1b[0m')
-    console.log('\x1b[33m', 'Names:', projectNames.join(', '), '\x1b[0m')
+    console.log('\x1b[33m', 'Names:', projectNames, '\x1b[0m')
     return
   }
 
@@ -25,7 +25,9 @@ function pullProject(projectsList) {
       console.log(err?.message)
     }
 
-    console.log(stdout)
+    if (stdout) {
+      console.log(stdout)
+    }
 
     if (!err) {
       console.log("\x1b[33m", '== PULL SUCCESS ==','\x1b[0m')
