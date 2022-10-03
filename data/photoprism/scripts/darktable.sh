2ef8211efd8eae98175ecfc7427f8d832b7eef30
Kirill Smirnov <krrllsm@gmail.com>
2022-10-03 19:11:18 +0400

    add pizm

--- data/photoprism/scripts/darktable.sh
+++ data/photoprism/scripts/darktable.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
 apt-get update
-apt-get install sudo gpg
-
-echo 'deb http://download.opensuse.org/repositories/graphics:/darktable/Debian_11/ /' | sudo tee /etc/apt/sources.list.d/graphics:darktable.list
-curl -fsSL https://download.opensuse.org/repositories/graphics:darktable/Debian_11/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/graphics_darktable.gpg > /dev/null
-
-apt-get update
 apt-get install darktable
