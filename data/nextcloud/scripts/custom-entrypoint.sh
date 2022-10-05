3822362557e9568f04c29ba88e57cb523c95f63c
Kirill Smirnov <krrllsm@gmail.com>
2022-10-06 01:18:31 +0400

    update

--- data/nextcloud/scripts/custom-entrypoint.sh
+++ data/nextcloud/scripts/custom-entrypoint.sh
@@ -1,13 +1,11 @@
 apt update
+
+# For Media DC
 apt install -y python3
 apt install -y python3-pip
 pip3 install numpy pillow scipy pywavelets pillow_heif cryptography pynacl hexhamming pywavelets
 
-python3 -m pip install -U pip
-arch=$(arch | sed s/x86_64/amd64/) && python3 -m pip install pywavelets
-
-echo '=======  === = =  = = = ========= ===================== = = = = = = = = === = = ======='
-
-add --no-cache ffmpeg imagemagick supervisor py3-numpy py3-pillow py3-asn1crypto
+# For fix warnings
 apt install -y libmagickcore-6.q16-6-extra ffmpeg
+
 /entrypoint.sh apache2-foreground
