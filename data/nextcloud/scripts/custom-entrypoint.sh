ea1e26741d0b27ead83c01c42f316ebd40a3888e
Kirill Smirnov <krrllsm@gmail.com>
2022-10-06 00:28:37 +0400

    update

---
+++ data/nextcloud/scripts/custom-entrypoint.sh
@@ -0,0 +1,8 @@
+apt update
+apt install -y libmagickcore-6.q16-6-extra ffmpeg
+/entrypoint.sh apache2-foreground
+add --no-cache ffmpeg imagemagick supervisor py3-numpy py3-pillow py3-asn1crypto
+apk add --no-cache py3-cffi py3-scipy py3-pynacl py3-cryptography py3-pip
+python3 -m pip install -U pip
+python3 -m pip install pillow_heif
+python3 -m pip install pywavelets
