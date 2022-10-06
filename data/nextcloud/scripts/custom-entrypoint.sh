cb39ebf8fa4dc88d67ecb1fb39ef3e786037558d
ksmi <krrllsm@gmail.com>
2022-10-06 20:37:14 +0400

    Update nextcloud (#22)
    update nextcloud entrypoint

---
+++ data/nextcloud/scripts/custom-entrypoint.sh
@@ -0,0 +1,11 @@
+apt update
+
+# For Media DC
+apt install -y python3
+apt install -y python3-pip
+pip3 install numpy pillow scipy pywavelets pillow_heif cryptography pynacl hexhamming pywavelets
+
+# For fix warnings
+apt install -y libmagickcore-6.q16-6-extra ffmpeg
+
+/entrypoint.sh apache2-foreground
