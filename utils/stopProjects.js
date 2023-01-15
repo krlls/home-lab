e150831cf8ad1a76870071bfbe7ed01834019e90
Kirill Smirnov <krrllsm@gmail.com>
2023-01-16 01:02:03 +0700

    fix

--- utils/stopProjects.js
+++ utils/stopProjects.js
@@ -6,6 +6,7 @@ const exec = require('child_process').exec
 function startProjects(componentsList) {
   console.log("\x1b[31m", '== COMPONENTS STOP ==','\x1b[0m')
   console.log('Components:', "\x1b[32m", componentsList.map((p) => p.name).join(', '),'\x1b[0m')
+
   const cmpStr = componentsList.reduce((previousValue, { file, permanently }) => {
     if (permanently) {
       return previousValue
