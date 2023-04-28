9b25af4ad8e0845e8700caf066fd692f33513cc0
Ksmi <krrllsm@gmail.com>
2023-04-28 19:23:42 +0300

    fix warnings

--- data/nextcloud/scripts/custom-entrypoint.sh
+++ data/nextcloud/scripts/custom-entrypoint.sh
@@ -1,4 +1,4 @@
-apt update
+apt-get update
 
 # For Media DC
 #apt install -y python3
@@ -6,6 +6,6 @@ apt update
 #pip3 install numpy pillow scipy pywavelets pillow_heif cryptography pynacl hexhamming pywavelets
 
 # For fix warnings
-apt install -y libmagickcore-6.q16-6-extra ffmpeg
+apt-get install -y libmagickcore-6.q16-6-extra ffmpeg
 
 /entrypoint.sh apache2-foreground
