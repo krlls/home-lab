e8000af363b25367c2056c1abc37184dbf6a59b2
ksmi <krrllsm@gmail.com>
2023-05-04 22:10:54 +0300

    Rate limit (#33)
    * rate limit
    
    * remove python packages
    
    * fix warnings

--- data/nextcloud/scripts/custom-entrypoint.sh
+++ data/nextcloud/scripts/custom-entrypoint.sh
@@ -1,11 +1,11 @@
-apt update
+apt-get update
 
 # For Media DC
-apt install -y python3
-apt install -y python3-pip
-pip3 install numpy pillow scipy pywavelets pillow_heif cryptography pynacl hexhamming pywavelets
+#apt install -y python3
+#apt install -y python3-pip
+#pip3 install numpy pillow scipy pywavelets pillow_heif cryptography pynacl hexhamming pywavelets
 
 # For fix warnings
-apt install -y libmagickcore-6.q16-6-extra ffmpeg
+apt-get install -y libmagickcore-6.q16-6-extra ffmpeg
 
 /entrypoint.sh apache2-foreground
